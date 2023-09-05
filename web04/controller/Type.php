f250555453cc6eedcc12f98119f9d3c7ec7bbfce
CBK <cbkcbkcbkcbk@yahoo.com.tw>
2023-09-05 16:34:07 +0800

    28完成

--- web04/controller/Type.php
+++ web04/controller/Type.php
@@ -1,17 +1,51 @@
 <?php include_once "DB.php";
-class Type extends DB{
+class Type extends DB
+{
     function __construct()
     {
         parent::__construct("types");
     }
-    function hasMid($id){
-        return $this->count(['big'=>$id]);
+    function hasMid($id)
+    {
+        return $this->count(['big' => $id]);
     }
-    function getMids($id){
-        return $this->all(['big'=>$id]);
+    function getMids($id)
+    {
+        return $this->all(['big' => $id]);
     }
-    function getBig($id){
-        $row=$this->find($id);
+    function getBig($id)
+    {
+        $row = $this->find($id);
         return $this->find($row['big']);
+    }
+    function type($id)
+    {
+        if ($id != 0) {
+            $row = $this->find($id);
+            $type = ($row['big'] == 0) ? "big" : "mid";
+        } else {
+            $type = 'all';
+        }
+        return $type;
+    }
+    function nav($id)
+    {
+        $type = $this->type($id);
+        $nav='';
+        switch ($type) {
+            case "all":
+                $nav="全部商品";
+                break;
+            case "big":
+                $row=$this->find($id);
+                $nav=$row['name'];
+                break;
+            case "mid":
+                $row=$this->find($id);
+                $big=$this->find($row['big']);
+                $nav=$big['name'].">".$row['name'];
+                break;
+        }
+        return $nav;
     }
 }
---
+++ web04/views/front/main.php
@@ -0,0 +1,4 @@
+<h2>
+<?php $type=$_GET['type']??0;
+echo $Type->nav($type);?>
+</h2>
